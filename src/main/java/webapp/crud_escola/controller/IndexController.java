0e3cbac63d05bef10cf4559de433e3185e112d0c
DRCreatorZ <111887448+DRCreatorZ@users.noreply.github.com>
2024-03-22 22:10:39 -0300

    Aula 22/03/24

--- src/main/java/webapp/crud_escola/controller/IndexController.java
+++ src/main/java/webapp/crud_escola/controller/IndexController.java
@@ -1,4 +1,4 @@
-package webapp.crud_escola.controller;
+package webapp.crud_escola.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
